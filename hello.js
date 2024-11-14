434b48ade386e310c7e6c65a9893aab94e7510c7
higgjack <higgjack@oregonstate.edu>
2024-11-13 16:26:46 -0800

    uhhh

--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
 console.log("hi")
+console.log(process.env)
+console.log(process.env.MY_ENV_VARIABLE)
+
+var figlet = require("figlet");
+
+
+figlet("Hello CS290", function (err, data) {
+  if (!err) {
+    console.log(data);
+  }
+  
+});
+
+var fs = require("fs")
+fs.readFile("hello.js",function(err,data){
+    if (!err){
+        console.log("==file contents:",data.toString())
+    }
+})
+console.log("after")
